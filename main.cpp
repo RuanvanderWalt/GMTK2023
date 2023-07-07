e2bdc8130ff539c6702a739a4bba750661da8edb
Simon <u22490729@tuks.co.za>
2023-07-07 20:27:38 +0200

    Prototype

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <vector>
 #include <iostream>
 
 const int SCREENWIDTH = 800;
@@ -7,35 +8,74 @@ const int SCREENHEIGHT = 450;
 class Player{
 private:
     Vector2 playerPos;
-
+    float speed = 10.0f;
 public:
+    Color color = RED;
+    Vector2 getPlayerPos() {
+        return playerPos;
+    }
+
+
     Player(){
         playerPos = {(float)SCREENWIDTH/2, (float)SCREENHEIGHT/2 };
     }
     void moveLeft(){
-        if(playerPos.x - 4.0 >= 40){
-            playerPos.x -= 4.0;
+        if(playerPos.x - speed >= 40){
+            playerPos.x -= speed;
         }
     }
     void moveRight(){
-        if(playerPos.x + 4.0 <= SCREENWIDTH - 40) {
-            playerPos.x += 4.0;
+        if(playerPos.x + speed <= SCREENWIDTH - 40) {
+            playerPos.x += speed;
         }
     }
     void moveUp(){
-        if(playerPos.y - 4.0 >= 40){
-            playerPos.y -= 4.0;
+        if(playerPos.y - speed >= 40){
+            playerPos.y -= speed;
         }
     }
     void moveDown(){
-        if(playerPos.y + 4.0 <= SCREENHEIGHT - 40){
-            playerPos.y += 4.0;
+        if(playerPos.y + speed <= SCREENHEIGHT - 40){
+            playerPos.y += speed;
         }
     }
     void drawPlayer(){
-        DrawCircle(playerPos.x, playerPos.y, 40, RED);
+        DrawCircle(playerPos.x, playerPos.y, 40, color);
     }
 };
+
+
+class attack {
+public:
+    Vector2 start;
+    Vector2 end;
+    int cooldown ;
+    attack(){
+        start={(float)(std::rand()%800),(float)(std::rand()%450)};
+        end={(float)(std::rand()%800),(float)(std::rand()%450)};
+        cooldown =120;
+    }
+
+    void draw() {
+        if (cooldown > 0){
+            DrawLineEx(start,end,30,GRAY);
+        } else {
+            DrawLineEx(start,end,30,RED);
+        }
+
+    }
+
+    void update(){
+        cooldown--;
+    }
+
+private:
+
+};
+
+
+
+
 
 int main(void)
 {
@@ -49,14 +89,14 @@ int main(void)
 
     SetTargetFPS(60);
 
+    std::vector<attack*> attacks;
+    attacks.push_back(new attack());
+    int attackTimerMax= 180;
+    int attackTimer =0;
+
 
     while (!WindowShouldClose())
     {
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
         if(IsKeyDown(KEY_W)){
             player.moveUp();
         }
@@ -70,9 +110,47 @@ int main(void)
             player.moveRight();
         }
 
+        attackTimer++;
+
+        if (attackTimer>attackTimerMax){
+            attackTimer = 0;
+            attacks.push_back(new attack());
+            attackTimerMax--;
+        }
+
+        bool collied = false;
+
+        for (int i=0;i<attacks.size();i++){
+            attacks[i]->update();
+            if (attacks[i]->cooldown < -30){
+                attacks.erase(std::next(attacks.begin(), i));
+                break;
+            }
+            if (!collied && attacks[i]->cooldown <0){
+                collied = CheckCollisionPointLine(player.getPlayerPos(),attacks[i]->start,attacks[i]->end,70);
+            }
+
+        }
+
+        if (collied){
+            player.color = GREEN;
+        }
+
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+
+
         DrawText("Move the player using the WASD keys", 10, 10, 24, DARKGRAY);
 
         player.drawPlayer();
+
+        for (int i=0;i<attacks.size();i++){
+            attacks[i]->draw();
+        }
+
 
         EndDrawing();
 
