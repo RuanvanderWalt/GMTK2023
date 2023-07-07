f37b37634c2e517f64543177790edb4166245e61
Simon <u22490729@tuks.co.za>
2023-07-07 19:35:44 +0200

    basic raylib

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <raylib.h>
 #include <iostream>
 
-int main() {
+int main(void)
+{
 
-    std::cout << "Hello World!" << std::endl;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+
+    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+             EndDrawing();
+
+    }
+
+
+    CloseWindow();
 
     return 0;
 }
